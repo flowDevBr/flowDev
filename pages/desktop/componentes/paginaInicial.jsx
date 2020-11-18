2ee7240e05f67478c3e78a34b7ec770eb30c1ad7
caio bueno <caioebueno@gmail.com>
2020-11-18 10:26:48 -0300

    done

--- pages/desktop/componentes/paginaInicial.jsx
+++ pages/desktop/componentes/paginaInicial.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import MainSvg from '../images/main';
 
 class nav extends Component {
     render() {
         return (
             <div className="container-paginainicial">
-                <div className="col">
-                    <div className="row" style={{width: 800, justifyContent: 'space-between'}}>
+                <div className="col" style={{alignItems: 'center'}}>
+                    <div className="row" style={{width: 800, justifyContent: 'space-between', marginBottom: 30}}>
                         <span className="container-paginainicial-titulo">Landing<br />
                             Template for <br />Startups</span>
                         <div className="row">
                             <span style={{ marginLeft: 60, marginTop: 30 }} className="container-paginas-texto">Our landing pagetemplate works on all<br /> devices, so you only have to set it up<br /> once, and get beatiful results forever. <br /><br />
                                 <button className="container-paginainicial-botao">Saber Mais</button></span></div>
+                    </div>
+                    <div style={{paddingLeft: 70,}}>
+                    <MainSvg />
                     </div>
                 </div>
             </div>
